38f04a6a934628f30663cbbb691693f56483a55b
Gabriel Yshay <Gabriel Yshay>
2017-05-09 21:18:24 -0300

    batalha added

---
+++ src/Batalha.java
@@ -0,0 +1,13 @@
+
+public class Batalha extends Controller{
+	public static void main(String[] args) {
+		Batalha b = new Batalha();
+		
+		//b.addEvent(); usa isso aqui pra add eventos
+		//ordem de eventos que acontecem em uma batalha:
+		/*T1 e T2 se apresentam
+		 * ai sla, vamo escolher isso aqui e começar a programar
+		 */
+		b.run();
+	}
+}

f3b95e78945b6f69487fd4e91b58caac37fea9e8
Lcastelo <lucas.castelo@usp.br>
2017-05-13 19:54:42 -0300

    Update Acao.java
    Coloquei os 2 if nojentos. Vc vai reclamar, mas... foi o melhor q eu pensei

--- src/Acao.java
+++ src/Acao.java
@@ -20,12 +20,31 @@ public class Acao extends Event{
 		}
 		else{
 			if(t1.atualVivo()){
+				if((t1.atualTipo()=="Agua" && t2.atualTipo()=="Fogo") ||(t1.atualTipo()=="Fogo" && t2.atualTipo()=="Grama") ||(t1.atualTipo()=="Grama" && t2.atualTipo()=="Agua") ||(t1.atualTipo()=="Agua" && t2.atualTipo()=="Terra") ||(t1.atualTipo()=="Terra" && t2.atualTipo()=="Eletrico") ||(t1.atualTipo()=="Eletrico" && t2.atualTipo()=="Agua") ||(t1.atualTipo()=="Psiquico" && t2.atualTipo()=="Lutador") ||(t1.atualTipo()=="Lutador" && t2.atualTipo()=="Dark") ||(t1.atualTipo()=="Dark" && t2.atualTipo()=="Psiquico")){
+					int n = (int)Math.floor(t1.totalAtaques*Math.random());
+					t2.atualMudaVida(-t1.ataqueN(2*n).damage);
+					
+					System.out.println("  >" + t1.nome + ": " + t1.nomeAtual() + " " + t1.ataqueN(n).description()
+							+ ". It's super effective." + "   :" + t1.nomeAtual() + t1.vidaAtual() + " " + t2.nomeAtual()+t2.vidaAtual());
+					System.out.println();
+				}
+				else if((t1.atualTipo()=="Fogo" && t2.atualTipo()=="Agua") ||(t1.atualTipo()=="Grama" && t2.atualTipo()=="Fogo") ||(t1.atualTipo()=="Agua" && t2.atualTipo()=="Grama") ||(t1.atualTipo()=="Terra" && t2.atualTipo()=="Agua") ||(t1.atualTipo()=="Eletrico" && t2.atualTipo()=="Terra") ||(t1.atualTipo()=="Agua" && t2.atualTipo()=="Eletrico") ||(t1.atualTipo()=="Lutador" && t2.atualTipo()=="Psiquico") ||(t1.atualTipo()=="Dark" && t2.atualTipo()=="Lutador") ||(t1.atualTipo()=="Psiquico" && t2.atualTipo()=="Dark")){
+					int n = (int)Math.floor(t1.totalAtaques*Math.random());
+					t2.atualMudaVida(-t1.ataqueN(n/2).damage);
+					
+					System.out.println("  >" + t1.nome + ": " + t1.nomeAtual() + " " + t1.ataqueN(n).description()
+							+ ". It's not very effective." +"   :" + t1.nomeAtual() + t1.vidaAtual() + " " + t2.nomeAtual()+t2.vidaAtual());
+					System.out.println();
+				}
+				
+				else{
 				int n = (int)Math.floor(t1.totalAtaques*Math.random());
 				t2.atualMudaVida(-t1.ataqueN(n).damage);
 				
 				System.out.println("  >" + t1.nome + ": " + t1.nomeAtual() + " " + t1.ataqueN(n).description()
 						+ "   :" + t1.nomeAtual() + t1.vidaAtual() + " " + t2.nomeAtual()+t2.vidaAtual());
 				System.out.println();
+				}
 			}
 		}
 
